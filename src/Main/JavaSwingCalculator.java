843f22b0238d7835c2016733935aefc7b2d1f68a
Gabriel de Souza <gabriel.dsouzapro@gmail.com>
2024-03-11 08:54:11 -0300

    feat: update meta datas and removed unnecessary comments

---
+++ src/Main/JavaSwingCalculator.java
@@ -0,0 +1,16 @@
+package Main;
+
+/**
+ *
+ * @author Gabriel de Souza
+ */
+public class JavaSwingCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
